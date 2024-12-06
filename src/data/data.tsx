cf4c2c72e257129d72e56db2e9adf94d363a2c5a
SyafiqMSI <muhammadsyafiqibra@gmail.com>
2024-12-06 15:49:59 +0700

    util(table): dummy for table

---
+++ src/data/data.tsx
@@ -0,0 +1,62 @@
+import { ArrowDown, ArrowRight, ArrowUp, CircleCheck, CircleIcon, CircleOffIcon, LucideMessageCircleQuestion, Timer } from "lucide-react"
+
+export const labels = [
+  {
+    value: "bug",
+    label: "Bug",
+  },
+  {
+    value: "feature",
+    label: "Feature",
+  },
+  {
+    value: "documentation",
+    label: "Documentation",
+  },
+]
+
+export const statuses = [
+  {
+    value: "backlog",
+    label: "Backlog",
+    icon: LucideMessageCircleQuestion,
+  },
+  {
+    value: "todo",
+    label: "Todo",
+    icon: CircleIcon,
+  },
+  {
+    value: "in progress",
+    label: "In Progress",
+    icon: Timer,
+  },
+  {
+    value: "done",
+    label: "Done",
+    icon: CircleCheck,
+  },
+  {
+    value: "canceled",
+    label: "Canceled",
+    icon: CircleOffIcon,
+  },
+]
+
+export const priorities = [
+  {
+    label: "Low",
+    value: "low",
+    icon: ArrowDown,
+  },
+  {
+    label: "Medium",
+    value: "medium",
+    icon: ArrowRight,
+  },
+  {
+    label: "High",
+    value: "high",
+    icon: ArrowUp,
+  },
+]
