c19bbb968b6ae28466c4a1c6c1bdeff5cafa8116
SyafiqMSI <muhammadsyafiqibra@gmail.com>
2025-03-16 13:34:09 +0700

    fix(config): gcp->hf

--- next.config.ts
+++ next.config.ts
@@ -1,15 +1,30 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
-  images: {
-    domains: ['35.197.141.36'], // Replace 'localhost' with your actual domain in production
-  },
-};
+// const nextConfig: NextConfig = {
+//   images: {
+//     domains: ['35.197.141.36'], // Replace 'localhost' with your actual domain in production
+//   },
+// };
 
-module.exports = {
-  images: {
-    domains: ['storage.googleapis.com'],
-  },
-};
+// module.exports = {
+//   images: {
+//     domains: ['storage.googleapis.com'],
+//   },
+// };
 
-export default nextConfig;
+// export default nextConfig;
+
+const nextConfig = {
+  images: {
+    domains: ['huggingface.co'],
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'huggingface.co',
+        pathname: '**',
+      },
+    ],
+  },
+}
+
+module.exports = nextConfig
